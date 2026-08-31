e586348db39ce9ad148f0d67ab76f8d993e3092e
Anupam Singh <anupamsssingh123@gmail.com>
2026-08-31 22:28:08 +0530

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,10 @@ import cookieparser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
-import userRoute from "./routes/user.route.js";
-import CompanyRoute from "./routes/company.route.js";
-import jobRoute from "./routes/job.route.js";
-import applicationRoute from "./routes/application.route.js";
+import userRoute from"./routes/user.route.js";
+import  CompanyRoute from "./routes/company.route.js";
+import  jobRoute from "./routes/job.route.js";
+import  applicationRoute from "./routes/application.route.js";
 import path from "path";
 
 dotenv.config({});
@@ -15,54 +15,35 @@ const app = express();
 
 const _dirname = path.resolve();
 
-// ================= MIDDLEWARE =================
 
+//middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended:true}));
 app.use(cookieparser());
-
-const corsOptions = {
-    origin: "http://localhost:5173",
-    credentials: true,
-};
-
+const corsOptions ={
+    origin:"http://localhost:5173",
+    credentials:true
+}
 app.use(cors(corsOptions));
 
-// ================= PORT =================
-
 const PORT = process.env.PORT || 3000;
+//apo's
+app.use("/api/v1/user",userRoute);
+app.use("/api/v1/company",CompanyRoute);
+app.use("/api/v1/job",jobRoute);
+app.use("/api/v1/application",applicationRoute);
 
-// ================= API ROUTES =================
-
-app.use("/api/v1/user", userRoute);
-app.use("/api/v1/company", CompanyRoute);
-app.use("/api/v1/job", jobRoute);
-app.use("/api/v1/application", applicationRoute);
-
-// ================= FRONTEND =================
-
-app.use(
-    express.static(
-        path.join(_dirname, "frontend", "dist")
-    )
-);
-
-// IMPORTANT: Express latest version ke liye
-// '*' ki jagah '/{*splat}'
-app.get("/{*splat}", (_, res) => {
-    res.sendFile(
-        path.resolve(
-            _dirname,
-            "frontend",
-            "dist",
-            "index.html"
-        )
-    );
+app.use(express.static(path.join(_dirname,"/frontend/dist")));
+app.get('*',(_,res) =>{
+    res.sendFile(path.resolve(_dirname,"frontend","dist","index.html"));
 });
 
-// ================= SERVER =================
 
-app.listen(PORT, () => {
+// "http//localhost:8000/api/v1/user/register"
+// "http//localhost:8000/api/v1/user/login"
+// "http//localhost:8000/api/v1/user/profile/update"
+
+app.listen(PORT,()=>{
     connectDB();
     console.log(`Server running at port ${PORT}`);
-});
+})
