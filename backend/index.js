81ea91129fcf1290dc58e9a7059c39e1e581625d
Anupam Singh <anupamsssingh123@gmail.com>
2026-09-01 09:47:09 +0530

    c On branch main
    esc

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,10 @@ import cookieparser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
-import userRoute from"./routes/user.route.js";
-import  CompanyRoute from "./routes/company.route.js";
-import  jobRoute from "./routes/job.route.js";
-import  applicationRoute from "./routes/application.route.js";
+import userRoute from "./routes/user.route.js";
+import CompanyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
 import path from "path";
 
 dotenv.config({});
@@ -15,35 +15,40 @@ const app = express();
 
 const _dirname = path.resolve();
 
-
-//middleware
+// Middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieparser());
-const corsOptions ={
-    origin:"https://jobportal-youtube-3.onrender.com",
-    credentials:true
-}
+
+const corsOptions = {
+    origin: [
+        "https://jobportal.buzz",
+        "https://www.jobportal.buzz",
+        "https://jobportal-youtube-3.onrender.com"
+    ],
+    credentials: true
+};
+
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
-//apo's
-app.use("/api/v1/user",userRoute);
-app.use("/api/v1/company",CompanyRoute);
-app.use("/api/v1/job",jobRoute);
-app.use("/api/v1/application",applicationRoute);
 
-app.use(express.static(path.join(_dirname,"/frontend/dist")));
-app.get('/*splat', (_, res) => {
-    res.sendFile(path.resolve(_dirname,"frontend","dist","index.html"));
+// APIs
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", CompanyRoute);
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
+
+// Serve frontend
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+
+app.get("/*splat", (_, res) => {
+    res.sendFile(
+        path.resolve(_dirname, "frontend", "dist", "index.html")
+    );
 });
 
-
-// "http//localhost:8000/api/v1/user/register"
-// "http//localhost:8000/api/v1/user/login"
-// "http//localhost:8000/api/v1/user/profile/update"
-
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     connectDB();
     console.log(`Server running at port ${PORT}`);
-})
+});
